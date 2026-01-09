fbeb5019e423f524b5ec249662b20ff757820d16
bipashasaha <bipashasaha544@gmail.com>
2026-01-09 09:32:53 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const navbarLinks = document.querySelector(".navbar-links");
+
+menu.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  menu.classList.toggle("animate");
+});
